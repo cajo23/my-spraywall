f6e5dc3a5f8aebd546cf8423a63091227d20c633
cajo23 <143487612+cajo23@users.noreply.github.com>
2024-04-09 16:52:27 +0200

    Navigation for links

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,36 @@
+import styles from './Navigation.module.css';
+import Link from 'next/link';
+
+const LINKS = [
+    {href: '/', text: 'Home', description: 'My project idea'},
+    {href: '/about', text: 'About', description: 'About this project'},
+    {href: '/tools', text: 'Tools', description: 'A few tools I built'},
+]
+
+export default function Navigation() {
+    return (<div className={styles.grid}>
+            
+            {/*<
+            
+            NavigationLink text= "Home" href="/" description= "My project idea"/>
+            <NavigationLink text= "About" href="/about" description= "About this project"/>
+            <NavigationLink text= "Tools" href="/tools" description= "A few tools I built"/>
+            
+            */}
+
+           {/* {LINKS.map( l => <NavigationLink key={l.href} text={l.text} href={l.href} description= {l.description}/>)} */}
+
+           {LINKS.map( l => <NavigationLink key={l.href} {...l}/>)}
+
+        </div>);
+}
+
+function NavigationLink({ text, href, description}){
+    return(<Link href={href}
+    className={styles.card}>
+    <h2>
+        {text} <span>-&gt;</span>
+    </h2>
+    <p>{description}</p>
+    </Link>)
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Navigation from "@/Components/Navigation/Navigation";
 
 export default function Home() {
   return (
     <main className={styles.main}>
 
-<div className={styles.grid}>
-        <a
-          href="/"
-          className={styles.card}
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Navigation />
 
-        <a
-          href="/"
-          className={styles.card}
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="/"
-          className={styles.card}
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="/"
-          className={styles.card}
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    
 
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          
+          <code className={styles.code}>Johanna Carlström</code>
         </p>
         <div>
           <a
-            href="/"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             By{" "}
             <Image
@@ -74,10 +37,10 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src="/climbing-holds.png"
           alt="Next.js Logo"
-          width={180}
-          height={37}
+          width={100}
+          height={100}
           priority
         />
       </div>
